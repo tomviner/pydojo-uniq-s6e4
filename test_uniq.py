b0ce0d8d22990233e2c1ebb2df4d7e3ffded9d44
Madda <nobody.youknow@local>
2014-12-04 20:33:02 +0000

    Failing test with combined diacritics

--- test_uniq.py
+++ test_uniq.py
@@ -1,8 +1,10 @@
+import pytest
+
 from uniq import uniq
 
-def test_basic_uniq():
-    with open('test1') as f:
+@pytest.mark.parametrize("n", range(1, 6))
+def test_basic_uniq(n):
+    with open('test{}'.format(n)) as f:
         output = uniq(f)
-    expected = open('expected1').read().splitlines()
+    expected = open('expected{}'.format(n)).read().splitlines()
     assert expected == output
-    
