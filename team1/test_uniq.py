439f2ef7796f479e5cc7341c269d7eb86933dc68
TomV <tom@viner.tv>
2014-12-04 23:16:48 +0000

    add tests for arg and stdin

--- team1/test_uniq.py
+++ team1/test_uniq.py
@@ -1,3 +1,5 @@
+from subprocess import check_output
+
 import pytest
 
 from uniq import uniq
@@ -8,3 +10,14 @@ def test_basic_uniq(n):
         output = uniq(f)
     expected = open('expected{}'.format(n)).read().splitlines()
     assert expected == output
+
+
+def test_arg():
+    # returns bytes
+    output = check_output(["./uniq.py", "./test1"])
+    assert open('expected1', 'bU').read() == output
+
+def test_stdin():
+    # returns bytes
+    output = check_output("cat ./test1 | ./uniq.py", shell=1)
+    assert open('expected1', 'bU').read() == output
