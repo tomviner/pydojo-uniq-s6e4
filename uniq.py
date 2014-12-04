2c16f135ab07f3b05de38d5a08bbf0e936ef9bae
Madda <nobody.youknow@local>
2014-12-04 20:48:49 +0000

    Uniq, and it works in windows!

--- uniq.py
+++ uniq.py
@@ -1,4 +1,7 @@
+#! /usr/bin/env python
 from unicodedata import normalize
+import sys
+import os
 
 
 def uniq(f):
@@ -9,3 +12,12 @@ def uniq(f):
             results.append(line)
             previous = normalize('NFKC', line)
     return results
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Syntax:')
+        print('uniq.py <filename>')
+        sys.exit(1)
+
+    target_file = sys.argv[1]
+    print(os.linesep.join(uniq(open(target_file))))
