f3a7e61e1b80763ae62ea33b5b5c64f4fd88d597
Madda <nobody.youknow@local>
2014-12-04 20:17:00 +0000

    It works, save now!

--- uniq.py
+++ uniq.py
@@ -1,2 +1,6 @@
-def uniq():
-    print('I am happy')
+def uniq(f):
+    results = []
+    for line in f.read().splitlines():
+        if (not results) or (results and line != results[-1]):
+            results.append(line)
+    return results
